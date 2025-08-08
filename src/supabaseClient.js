5d3962570cc66feec75b3440cde0b917fc6be14c
Kristian2002 <Kristianxhani20@gmail.com>
2025-08-08 10:44:26 +0200

    Nascondi le chiavi .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Sostituisci con i tuoi valori
-const supabaseUrl = 'https://bbcoxisizpjdixnwaltu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiY294aXNpenBqZGl4bndhbHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3ODQ0NjEsImV4cCI6MjA2OTM2MDQ2MX0.rYO2tF_xWWXnd97l1CQ2CY25wYxg_tr1P7jcAAUre5I'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
